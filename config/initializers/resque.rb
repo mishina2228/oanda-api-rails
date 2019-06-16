bb44dda6163a2e6e48209acdab631e095578aa36
mishina <temma182008@gmail.com>
2019-06-16 11:37:30 +0900

    try resque part2

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,4 @@
 Resque.redis = 'localhost:6379'
 Resque.redis.namespace = "resque:oanda_api_rails:#{Rails.env}"
+Resque.logger = Logger.new('log/resque.log', 5, 10.megabytes)
+Resque.logger.level = Logger::INFO
