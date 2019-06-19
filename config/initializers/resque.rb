8feb793201f217ace9e49784fa6e4648d34eaad3
mishina <temma182008@gmail.com>
2019-06-19 22:53:51 +0900

    fix a script, small changes

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,7 +6,3 @@ Resque.redis = 'localhost:6379'
 Resque.redis.namespace = "resque:oanda_api_rails:#{Rails.env}"
 Resque.logger = Logger.new('log/resque.log', 5, 10.megabytes)
 Resque.logger.level = Logger::INFO
-# The schedule doesn't need to be stored in a YAML, it just needs to
-# be a hash.  YAML is usually the easiest.
-# https://github.com/resque/resque-scheduler/issues/541#issuecomment-218611385
-Resque.schedule = YAML.load_file(Rails.root.join('config', 'resque_schedule.yml'))
--- lib/scripts/start_rails.sh
+++ lib/scripts/start_rails.sh
@@ -1,1 +1,2 @@
+cd /home/pi/git-repos/oanda-api-rails
 bundle exec rails s -b 192.168.1.12 -d
