43d397acc5145d349b100de4e0ce9acfb75a8985
mishina <temma182008@gmail.com>
2019-06-19 23:14:32 +0900

    revert default candles count on candle_job

--- app/jobs/candle_job.rb
+++ app/jobs/candle_job.rb
@@ -17,7 +17,7 @@ class CandleJob
 
     start = params[:start] || klass.latest_candle.time + klass::TIME_RANGE
     finish = params[:finish] || Time.zone.now
-    count = params[:count] || 100
+    count = params[:count] || 5000
     klass.save_numerous_candles(start: start, finish: finish, count: count)
   end
 
