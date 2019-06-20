38fac201699b3d7fadce8e5d93f7f9b4adebb705
mishina <temma182008@gmail.com>
2019-06-20 15:26:09 +0900

    set resque scheduler logfile path as an environment variable

--- lib/scripts/start_resque.sh
+++ lib/scripts/start_resque.sh
@@ -1,3 +1,3 @@
 cd /home/pi/git-repos/oanda-api-rails
 QUEUE=normal bundle exec rake environment resque:work &
-DYNAMIC_SCHEDULE=true PIDFILE=./tmp/pids/resque-scheduler.pid BACKGROUND=yes bundle exec rake environment resque:scheduler
+DYNAMIC_SCHEDULE=true PIDFILE=./tmp/pids/resque-scheduler.pid BACKGROUND=yes LOGFILE=./log/resque-scheduler.log bundle exec rake environment resque:scheduler
