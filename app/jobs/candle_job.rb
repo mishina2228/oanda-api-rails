3620fc90dd6c1ae6e305549007802f3fc6066787
mishina <temma182008@gmail.com>
2019-06-20 14:17:00 +0900

    process segmentation for job

--- app/jobs/candle_job.rb
+++ app/jobs/candle_job.rb
@@ -1,7 +1,16 @@
 class CandleJob
   def self.before_enqueue(params = {})
+    params = params.with_indifferent_access
     jobs = Resque.peek(@queue, 0, 100)
-    jobs.none? {|job| job['class'] == name}
+    jobs.each do |job|
+      next if job['class'] != name
+
+      args = job['args'].first || {}
+      if params['start'] == args['start'] && params['finish'] == args['finish'] && params['count'] == args['count']
+        return false
+      end
+    end
+    true
   end
 
   def self.perform(klass, params = {})
@@ -9,11 +18,46 @@ class CandleJob
     raise 'class must be some candle-ish' unless klass.include?(CandleConcern)
 
     params = params.with_indifferent_access
+    start, finish, count = arrange_params(klass, params)
+    if start >= finish
+      Rails.logger.info '開始時刻が終了時刻以前であるため終了します。'
+      Rails.logger.info "start: #{start}, finish: #{finish}"
+      return
+    end
 
-    start = params[:start] || klass.latest_candle.time + klass::TIME_RANGE
-    finish = params[:finish] || Time.zone.now
-    count = params[:count] || 5000
-    klass.save_numerous_candles(start: start, finish: finish, count: count)
+    ret = klass.gimme_candle(start: start, count: count)
+    if ret.present?
+      Rails.logger.info "#{ret.last.time} まで取得しました。"
+      start = ret.last.time + klass.const_get(:TIME_RANGE)
+    else
+      start += count * klass.const_get(:TIME_RANGE)
+    end
+
+    if start >= finish
+      Rails.logger.info "finish: #{finish.in_time_zone('Asia/Tokyo')} を超えたので終了します。"
+      return
+    end
+
+    Rails.logger.info "次の取得を開始: start = #{start}"
+    Resque.enqueue_in_with_queue(
+      @queue, 5.seconds, self.class,
+      start: start, finish: finish, count: count
+    )
+  end
+
+  def self.arrange_params(klass, params)
+    start = if params[:start]
+              params[:start].to_time
+            else
+              klass.latest_candle.time + klass::TIME_RANGE
+            end
+    finish = if params[:finish]
+               params[:finish].to_time
+             else
+               Time.zone.now
+             end
+    count = (params[:count] || 5000).to_i
+    [start, finish, count]
   end
 
   # startからfinishの期間がすべて休場時間であるか判定
