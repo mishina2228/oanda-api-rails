e1ed882d801fe370a5d63e876b16166a5eb31072
mishina <temma182008@gmail.com>
2019-06-20 15:15:30 +0900

    add some hint messages

--- lib/scripts/restart_applications.sh
+++ lib/scripts/restart_applications.sh
@@ -1,6 +1,9 @@
 cd /home/pi/git-repos/oanda-api-rails
 sh ./lib/scripts/finish_rails.sh
 sh ./lib/scripts/finish_resque.sh
+echo 'stopped rails and resque.'
+echo 'sleep 5 seconds ...'
 sleep 5s
 sh ./lib/scripts/start_resque.sh
 sh ./lib/scripts/start_rails.sh
+echo 'started rails and resque.'
