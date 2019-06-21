3ff8c3c8e8eb3c163980f120e61f1ed0ee89ea4c
mishina <temma182008@gmail.com>
2019-06-22 00:30:43 +0900

    suppress db query log

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.log_level = :info
+
   # log rotation
   config.logger = Logger.new('log/development.log', 5, 10.megabytes)
 
