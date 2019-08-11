4b2f6af771f9c61b6271a6b388786aaf77fd9115
mishina <temma182008@gmail.com>
2019-08-11 09:16:06 +0900

    acquire ip address automatically

--- lib/scripts/start_rails.sh
+++ lib/scripts/start_rails.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/pi/git-repos/oanda-api-rails
 bundle exec rake assets:precompile RAILS_ENV=production
-bundle exec rails s -b 192.168.1.12 -e production -d
+bundle exec rails s -b `hostname -I` -e production -d
