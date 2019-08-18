65aa803e32b3d68aed82b893a9fd23e925fe5c7b
mishina <temma182008@gmail.com>
2019-08-18 10:03:12 +0900

    fix ip address acquisition

--- lib/scripts/start_rails.sh
+++ lib/scripts/start_rails.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/pi/git-repos/oanda-api-rails
 bundle exec rake assets:precompile RAILS_ENV=production
-bundle exec rails s -b `hostname -I` -e production -d
+bundle exec rails s -b `ip -4 a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'` -e production -d
