30ee96524ad7cd7507efd20912da294120358151
mishina <temma182008@gmail.com>
2019-09-13 19:14:54 +0900

    delete unnecessary script files

--- lib/scripts/finish_resque.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-pgrep -f resque- | xargs kill
--- lib/scripts/start_resque.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd `dirname $0`/../..
-
-echo 'starting resque.'
-
-QUEUE=normal RAILS_ENV=production bundle exec rake environment resque:work &
-DYNAMIC_SCHEDULE=true PIDFILE=./tmp/pids/resque-scheduler.pid BACKGROUND=yes LOGFILE=./log/resque-scheduler.log RAILS_ENV=production bundle exec rake environment resque:scheduler
-
-echo 'started resque.'
