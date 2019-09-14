2f73152c54a187071d94763850e0fe69f70fce7c
mishina <temma182008@gmail.com>
2019-09-14 10:54:52 +0900

    review log settings

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,9 +4,4 @@ require 'resque/scheduler/server'
 
 Resque.redis = 'localhost:6379'
 Resque.redis.namespace = "resque:oanda_api_rails:#{Rails.env}"
-Resque.logger = Logger.new('log/resque.log', 5, 10.megabytes)
-Resque.logger.level = Logger::INFO
 Resque::Scheduler.dynamic = true
-Resque::Scheduler.configure do |c|
-  c.logfile = Rails.root.join('log', 'resque-scheduler.log')
-end
