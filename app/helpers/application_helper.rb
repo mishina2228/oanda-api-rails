61c4b0096bc9f1b1c6d22600c5cf42c43df7284f
mishina <temma182008@gmail.com>
2019-09-23 00:01:36 +0900

    improved display of ResqueScheduler index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def candle_latest_time(candle)
     candle ? candle.time : '(なし)'
   end
+
+  def check_icon(boolean)
+    boolean ? 'glyphicon-check' : 'glyphicon-unchecked'
+  end
 end
