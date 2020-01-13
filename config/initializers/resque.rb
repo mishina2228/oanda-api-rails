08b6f51dad20ba1e6c6fd2a1f24092747e6586ec
mishina <temma182008@gmail.com>
2020-01-13 22:48:45 +0900

    redis is not required on the test environment

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,14 +1,16 @@
-require 'resque'
-require 'resque-scheduler'
-require 'resque/scheduler/server'
-require 'resque/failure/multiple'
-require 'resque/failure/redis'
-require 'resque/failure/email_notification'
+unless Rails.env.test?
+  require 'resque'
+  require 'resque-scheduler'
+  require 'resque/scheduler/server'
+  require 'resque/failure/multiple'
+  require 'resque/failure/redis'
+  require 'resque/failure/email_notification'
 
-Resque.redis = 'localhost:6379'
-Resque.redis.namespace = "resque:oanda_api_rails:#{Rails.env}"
-Resque::Scheduler.dynamic = true
+  Resque.redis = 'localhost:6379'
+  Resque.redis.namespace = "resque:oanda_api_rails:#{Rails.env}"
+  Resque::Scheduler.dynamic = true
 
-Resque::Failure::Multiple.configure do |multi|
-  multi.classes = [Resque::Failure::Redis, Resque::Failure::EmailNotification]
+  Resque::Failure::Multiple.configure do |multi|
+    multi.classes = [Resque::Failure::Redis, Resque::Failure::EmailNotification]
+  end
 end
