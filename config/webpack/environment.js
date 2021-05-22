08dde8025507e236ec4017dcd7c0149570ceceb5
mishina <temma182008@gmail.com>
2021-05-22 23:18:01 +0900

    [WIP][ci skip] Upgrade to Bootstrap5
    * Add @popperjs/core
    * Remove popper.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,8 +12,7 @@ const webpack = require('webpack')
 environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    jQuery: 'jquery'
   })
 )
 
