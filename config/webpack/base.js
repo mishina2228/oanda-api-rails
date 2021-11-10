54d2293b9c04fc5d2ecab02c405daab85ed3448d
mishina <temma182008@gmail.com>
2021-11-10 22:52:07 +0900

    fix: expose jQuery global

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,6 +1,6 @@
 const { webpackConfig, merge } = require('@rails/webpacker')
 
-// https://github.com/rails/webpacker/issues/2844
+// quoted from https://github.com/rails/webpacker/issues/2844#issuecomment-753489020
 webpackConfig.module.rules.map(module => {
   if (module.test && module.test.toString().includes('scss')) {
     module.use.splice(-1, 0, {
@@ -20,4 +20,16 @@ const customConfig = {
   }
 }
 
-module.exports = merge(webpackConfig, customConfig)
+// quoted from https://github.com/rails/webpacker/issues/2845#issuecomment-755053711
+const jquery = require('./loaders/jquery')
+const webpack = require('webpack')
+
+webpackConfig.plugins.unshift(
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+)
+
+module.exports = merge(webpackConfig, customConfig, jquery)
