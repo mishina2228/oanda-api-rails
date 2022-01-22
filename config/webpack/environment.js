f8726c321edefc8565544a52e29fcc339ea1f6a2
mishina <temma182008@gmail.com>
2022-01-22 23:21:18 +0900

    refactor: Remove resolve-url-loader
    resolve-url-loader was needed to display Bootstrap3's glyphicon, but Bootstrap5 is used now.
    `url()` function in scss is also not used, so resolve-url-loader is no longer needed.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,4 @@
 const { environment } = require('@rails/webpacker')
 
-const sassLoader = environment.loaders.get('sass')
-sassLoader.use.splice(-1, 0, {
-  loader: 'resolve-url-loader'
-})
-
 environment.splitChunks()
 module.exports = environment
