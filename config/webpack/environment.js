1aaa0b3af3418e211cf92fbc04fff8e918a13984
mishina <temma182008@gmail.com>
2022-01-22 18:48:04 +0900

    refactor: Remove rails-erb-loader
    Because this project does not have any .js.erb files

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,9 @@
 const { environment } = require('@rails/webpacker')
-const erb = require('./loaders/erb')
 
 const sassLoader = environment.loaders.get('sass')
 sassLoader.use.splice(-1, 0, {
   loader: 'resolve-url-loader'
 })
-environment.loaders.prepend('erb', erb)
 
 environment.splitChunks()
 module.exports = environment
