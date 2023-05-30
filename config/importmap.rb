3c021b361c1f010b1bb09816fcd497422210f4bb
mishina <temma182008@gmail.com>
2023-05-30 19:00:03 +0900

    chore: Bump @popperjs/core from 2.11.7 to 2.11.8

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,7 +3,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/dist/esm/index.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/esm/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@fortawesome/fontawesome-svg-core", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.4.0/index.mjs"
