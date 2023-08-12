53000ce389e6fb4f779041cfd778dde5d882304e
mishina <temma182008@gmail.com>
2023-08-12 10:57:50 +0900

    chore: Bump bootstrap from 5.3.0 to 5.3.1

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@
 
 pin "application", preload: true
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/esm/index.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.1/dist/js/bootstrap.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@fortawesome/fontawesome-svg-core", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-svg-core@6.4.2/index.mjs"
 pin "@fortawesome/free-regular-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.4.2/index.mjs"
